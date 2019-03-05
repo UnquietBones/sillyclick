cb2a9de0d4f0ee9b548009ded7ff00b6058bf324
UnquietBones <35631425+UnquietBones@users.noreply.github.com>
2019-03-04 22:03:33 -0500

    Changing some things around for the Carrots Now that I have an idea of what I want to do, it's time to play around with the first set of buttons and layout. Looks like I really need to learn how this darned UI thing works...

--- src/sillyclick/GameWindow.java
+++ src/sillyclick/GameWindow.java
@@ -7,10 +7,10 @@ import java.awt.event.ActionEvent;
 
 public class GameWindow extends JFrame {
 
-    private int numFirstClicks = 0;
-    private int numSecondClicks = 0;
     TextField firstText = new TextField(10);
     TextField secondText = new TextField(10);
+    private int numFirstClicks = 0;
+    private int numStoredCarrots = 0;
 
     public GameWindow(String newTitle, int newWidth, int newHeight) {
         setTitle(newTitle);
@@ -22,32 +22,32 @@ public class GameWindow extends JFrame {
         getContentPane().add(panel);
 
         firstText.setBounds(70, 60, 100, 15);
-        secondText.setBounds(70, 120, 100, 15);
+        firstText.setText("0/10 carrots");
+
+        secondText.setBounds(170, 160, 100, 15);
+        secondText.setText("0 stored carrots");
 
         panel.setLayout(new FlowLayout());
 
-        JButton firstButton = new JButton("Click Me");
+        JButton firstButton = new JButton("Plant a Carrot");
         firstButton.setBounds(50, 60, 10, 30);
         firstButton.addActionListener(this::firstClick);
 
-        JButton secondButton = new JButton("Click Me");
-        secondButton.setBounds(50, 120, 10, 30);
-        secondButton.addActionListener(this::secondClick);
-
         panel.add(firstButton);
         panel.add(firstText);
-        panel.add(secondButton);
         panel.add(secondText);
     }
 
     public void firstClick(ActionEvent e) {
         numFirstClicks++;
-        firstText.setText("Clicks " + numFirstClicks);
-    }
 
-    public void secondClick(ActionEvent e) {
-        numSecondClicks++;
-        secondText.setText("Clicks " + numSecondClicks);
+        if (numFirstClicks == 10) {
+            firstText.setText(numFirstClicks + "/10 carrots");
+            // wait x seconds
+            numStoredCarrots += 10;
+            secondText.setText(numStoredCarrots + " stored carrots");
+            numFirstClicks = 0;
+        }
+        firstText.setText(numFirstClicks + "/10 carrots");
     }
-
 }
